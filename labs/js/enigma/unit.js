be9882fc4a375831dd3b27f735d179efa53cf3ff
Mike Koss <mckoss@startpad.org>
2010-01-10 12:27:20 -0800

    checkpoint - vector copy

--- labs/js/enigma/unit.js
+++ labs/js/enigma/unit.js
@@ -14,149 +14,15 @@
 // UnitTest - Each unit test calls a function which in turn calls
 // back Assert's on the unit test object.
 
-Function.prototype.FnMethod = function(obj)
-{
-	var _fn = this;
-	return function () { return _fn.apply(obj, arguments); };
-};
+global_namespace.Define('startpad.unit', function (NS)
+	Timer = global_namespace('startpad.timer');
 
-Function.prototype.FnArgs = function()
-{
-	var _fn = this;
-	var _args = [];
-	for (var i = 0; i < arguments.length; i++)
-		_args.push(arguments[i]);
-
-	return function () {
-		var args = [];
-		// In case this is a method call, preserve the "this" variable
-		var self = this;
-
-		for (var i = 0; i < arguments.length; i++)
-			args.push(arguments[i]);
-		for (i = 0; i < _args.length; i++)
-			args.push(_args[i]);
-
-		return _fn.apply(self, args);
-	};	
-};
-
-var UT = {
-
-// Extend(dest, src1, src2, ... )
-// Shallow copy properties in turn into dest object
-Extend: function(dest)
+NS.Extend(NS, {
+DW: function(st)
 	{
-	for (var i = 1; i < arguments.length; i++)
-		{
-		var src = arguments[i];
-		for (var prop in src)
-			{
-			if (src.hasOwnProperty(prop))
-				dest[prop] = src[prop];
-			}
-		}
-	},
-
-DW: function(st) {document.write(st);},
-
-Browser:
-	{
-	version: parseInt(navigator.appVersion),
-	fIE: navigator.appName.indexOf("Microsoft") != -1
-	},
-	
-// Convert all top-level object properties into a URL query string.
-// {a:1, b:"hello, world"} -> "?a=1&b=hello%2C%20world"
-StParams: function(obj)
-	{
-	if (obj == undefined || obj == null)
-		return "";
-		
-	var stDelim = "?";
-	var stParams = "";
-	for (var prop in obj)
-		{
-		if (!obj.hasOwnProperty(prop) || prop == "_anchor")
-			continue;
-		stParams += stDelim;
-		stParams += encodeURIComponent(prop);
-		// BUG: This is a bit bogus to encode a query param in JSON
-		if (typeof obj[prop] == "object")
-			stParams += "=" + encodeURIComponent(PF.EncodeJSON(obj[prop], true));
-		else if (obj[prop] != null)
-			stParams += "=" + encodeURIComponent(obj[prop]);
-		stDelim = "&";
-		}
-	if (obj._anchor)
-		stParams += "#" + encodeURIComponent(obj._anchor);
-	return stParams;
+	document.write(st);
 	}
-};  // UT
-
-UT.Timer = function(fnCallback, ms)
-{
-	this.ms = ms;
-	this.fnCallback = fnCallback;
-	return this;
-};
-
-UT.Timer.prototype = {
-	constructor: UT.Timer,
-	fActive: false,
-	fRepeat: false,
-	fInCallback: false,
-	fReschedule: false,
-
-Repeat: function(f)
-{
-	if (f == undefined)
-		f = true;
-	this.fRepeat = f;
-	return this;
-},
-
-Ping: function()
-{
-	// In case of race condition - don't call function if deactivated
-	if (!this.fActive)
-		return;
-
-	// Eliminate re-entrancy - is this possible?
-	if (this.fInCallback)
-		{
-		this.fReschedule = true;
-		return;
-		}
-
-	this.fInCallback = true;
-	this.fnCallback();
-	this.fInCallback = false;
-
-	if (this.fActive && (this.fRepeat || this.fReschedule))
-		this.Active(true);
-},
-
-// Calling Active resets the timer so that next call to Ping will be in this.ms milliseconds from NOW
-Active: function(fActive)
-{
-	if (fActive == undefined)
-		fActive = true;
-	this.fActive = fActive;
-	this.fReschedule = false;
-
-	if (this.iTimer)
-		{
-		clearTimeout(this.iTimer);
-		this.iTimer = undefined;
-		}
-
-	if (fActive)
-		this.iTimer = setTimeout(this.Ping.FnMethod(this), this.ms);
-
-	return this;
-}
-}; // UT.Timer
+});
 
 UT.UnitTest = function (stName, fn)
 {
