d2ca541491e7a96ec194e66d7b054693ff1154af
Mike Koss <mckoss@startpad.org>
2010-01-09 17:34:29 -0800

    setup crypto library

---
+++ labs/js/enigma/crypto.js
@@ -0,0 +1,15 @@
+/* crypto.js
+
+   Cryptographic utilities.
+   
+
+*/
+global_namespace.Define('startpad.crypto', function (NS)
+{
+NS.Extend(NS, {
+Entropy: function(s)
+	{
+	return 0;
+	}
+});
+});
