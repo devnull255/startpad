e7d008264a6183bfe3c945cb2015099dad6f8bf4
Mike Koss <mckoss@startpad.org>
2010-02-02 17:59:26 -0800

    checkpoint - more settings

--- labs/java/android/enigma/gen/org/startpad/android/enigma/R.java
+++ labs/java/android/enigma/gen/org/startpad/android/enigma/R.java
@@ -9,6 +9,8 @@ package org.startpad.android.enigma;
 
 public final class R {
     public static final class array {
+        public static final int alpha=0x7f040002;
+        public static final int reflector_names=0x7f040001;
         public static final int rotor_names=0x7f040000;
     }
     public static final class attr {
@@ -19,13 +21,21 @@ public final class R {
     public static final class id {
         public static final int settings=0x7f060001;
         public static final int sim=0x7f060000;
-        public static final int spn_rotors_1=0x7f060002;
+        public static final int spn_reflector=0x7f060002;
+        public static final int spn_rings_1=0x7f060006;
+        public static final int spn_rings_2=0x7f060007;
+        public static final int spn_rings_3=0x7f060008;
+        public static final int spn_rotors_1=0x7f060003;
+        public static final int spn_rotors_2=0x7f060004;
+        public static final int spn_rotors_3=0x7f060005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int rotor_prompt=0x7f050001;
+        public static final int reflector_prompt=0x7f050001;
+        public static final int ring_prompt=0x7f050003;
+        public static final int rotor_prompt=0x7f050002;
     }
 }
