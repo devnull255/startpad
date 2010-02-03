66062c9115767066266d7b94b9c0995020ce865a
Mike Koss <mckoss@startpad.org>
2010-02-03 15:36:21 -0800

    enforce unique rotor selection

--- labs/java/android/enigma/src/org/startpad/android/enigma/Enigma.java
+++ labs/java/android/enigma/src/org/startpad/android/enigma/Enigma.java
@@ -6,15 +6,24 @@ import org.startpad.android.enigma.R;
 
 import android.app.TabActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class Enigma extends TabActivity {
+public class Enigma extends TabActivity
+	{
+	private static final String TAG = "Enigma";
+    Spinner[] aspnRotors = new Spinner[3];
+    int[] aRotors = new int[] {0,1,2};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    	{
         super.onCreate(savedInstanceState);
         TabHost tabHost = getTabHost();
         
@@ -33,20 +42,55 @@ public class Enigma extends TabActivity {
         
         ArrayAdapter<CharSequence> adapter;
         
-        adapter = ArrayAdapter.createFromResource(
-                this, R.array.rotor_names, android.R.layout.simple_spinner_item);
+        adapter = ArrayAdapter.createFromResource(this, R.array.rotor_names, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         
-        ((Spinner) findViewById(R.id.spn_rotors_1)).setAdapter(adapter);
-        ((Spinner) findViewById(R.id.spn_rotors_2)).setAdapter(adapter);
-        ((Spinner) findViewById(R.id.spn_rotors_3)).setAdapter(adapter);
+
         
-        adapter = ArrayAdapter.createFromResource(
-                this, R.array.alpha, android.R.layout.simple_spinner_item);
+        for (int i = 0; i < 3; i++)
+        	{
+        	aspnRotors[i] = (Spinner) findViewById(R.id.spn_rotors_1 + i);
+        	aspnRotors[i].setAdapter(adapter);
+        	aspnRotors[i].setSelection(aRotors[i]);
+        	
+        	aspnRotors[i].setOnItemSelectedListener(
+            		new OnItemSelectedListener()
+            			{
+    					public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+    						{
+    						int iMe;
+    						for (iMe = 0; iMe < 3; iMe++)
+    							if (aspnRotors[iMe] == parent)
+    								break;
+
+    						Log.d(TAG, "Selected:" + iMe + " to:"+ id);
+    						
+    						// If rotor already used - swap it out with this rotor's value
+    						for (int i = 0; i < 3; i++)
+	    						{
+    							if (i == iMe)
+    								continue;
+    							 if (aRotors[i] == id)
+    							 	{
+    								aspnRotors[i].setSelection(aRotors[iMe]);
+    								aRotors[i] = aRotors[iMe];
+    							 	}
+	    						}
+    						
+    						aRotors[iMe] = (int) id;
+    						
+    						Log.d(TAG, "Rotors: " + aRotors[0] + ", " + aRotors[1] + ", " + aRotors[2]);
+    						}
+
+    					public void onNothingSelected(AdapterView<?> arg0) {}
+            			});
+        	}
+        	
+        adapter = ArrayAdapter.createFromResource(this, R.array.alpha, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         
-        ((Spinner) findViewById(R.id.spn_rings_1)).setAdapter(adapter);
-        ((Spinner) findViewById(R.id.spn_rings_2)).setAdapter(adapter);
-        ((Spinner) findViewById(R.id.spn_rings_3)).setAdapter(adapter);
-    }
-}
+        for (int i = 0; i < 3; i++)
+        	((Spinner) findViewById(R.id.spn_rings_1 + i)).setAdapter(adapter);
+
+    	}
+	}
