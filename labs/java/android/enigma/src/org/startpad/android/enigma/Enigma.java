baf748cc062e378df2e70ab37ce1f7825a380c8c
Mike Koss <mckoss@startpad.org>
2010-02-02 17:12:38 -0800

    checkpoint - enigma settings layout

--- labs/java/android/enigma/src/org/startpad/android/enigma/Enigma.java
+++ labs/java/android/enigma/src/org/startpad/android/enigma/Enigma.java
@@ -1,10 +1,14 @@
 package org.startpad.android.enigma;
 
+import java.util.List;
+
 import org.startpad.android.enigma.R;
 
 import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TabHost;
 
 public class Enigma extends TabActivity {
@@ -14,6 +18,8 @@ public class Enigma extends TabActivity {
         super.onCreate(savedInstanceState);
         TabHost tabHost = getTabHost();
         
+        // Setup top-level tabbled layout screen 
+        
         LayoutInflater.from(this).inflate(R.layout.main, tabHost.getTabContentView(), true);
 
         tabHost.addTab(tabHost.newTabSpec("sim")
@@ -22,5 +28,13 @@ public class Enigma extends TabActivity {
         tabHost.addTab(tabHost.newTabSpec("settings")
                 .setIndicator("Settings")
                 .setContent(R.id.settings));
+        
+        // Initializing the Settings view
+        
+        Spinner s1 = (Spinner) findViewById(R.id.spn_rotors_1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.rotor_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s1.setAdapter(adapter);
     }
 }
