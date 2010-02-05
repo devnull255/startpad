5fbb80ce0d69fb73a57a5ed74199bba4ea56b4cf
Mike Koss <mckoss@startpad.org>
2010-02-05 11:44:04 -0800

    checkpoint - start of Engima.js port

--- labs/java/android/enigma/src/org/startpad/Enigma.java
+++ labs/java/android/enigma/src/org/startpad/Enigma.java
@@ -1,9 +1,52 @@
+/*
+  Enigma.java - Enigma machine simulation.
+
+  Ported from Enigma.js, Feb, 2010
+
+  References:
+   	  Paper Enigma - http://mckoss.com/Crypto/Enigma.htm
+   	  Enigma Simulator - http://bit.ly/enigma-machine
+   	  Enigma History - http://en.wikipedia.org/wiki/Enigma_machine
+
+
+ */
+
 package org.startpad;
 
-public class Enigma {
+public class Enigma
+{
+	static class Rotor
+	{
+		String name;
+		String wires;
+		char notch;
+		
+		public Rotor() {}
+		
+		public Rotor(String name, String wires, char notch)
+			{
+			this.name = name;
+			this.wires = wires;
+			this.notch = notch;
+			}
+	}
+	
+	static Rotor[] rotors =
+		{
+		new Rotor("I", "EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q'),
+		new Rotor("II", "AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E'),
+		new Rotor("III", "BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V'),
+		new Rotor("IV", "ESOVPZJAYQUIRHXLNFTGKDCMWB", 'J'),
+		new Rotor("V", "VZBRGITYUPSDNHLXAWMJQOFECK", 'Z')
+		};
+
 	public static void main(String[] args)
 		{
-		System.out.println("Hello, world");
+			System.out.println("Hello, world");
+			for (int i = 0; i < rotors.length; i++)
+				System.out.println(rotors[i].name + ": " + rotors[i].wires + "(" +
+					rotors[i].notch + ")");
 		}
 
 }
+
