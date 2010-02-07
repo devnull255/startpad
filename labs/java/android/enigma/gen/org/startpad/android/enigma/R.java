5102d0e06b4fb64027ce31fac6d93e7f6c499aaa
Mike Koss <mckoss@startpad.org>
2010-02-07 15:03:36 -0800

    Sound effects

--- labs/java/android/enigma/gen/org/startpad/android/enigma/R.java
+++ labs/java/android/enigma/gen/org/startpad/android/enigma/R.java
@@ -9,9 +9,9 @@ package org.startpad.android.enigma;
 
 public final class R {
     public static final class array {
-        public static final int alpha=0x7f040002;
-        public static final int reflector_names=0x7f040001;
-        public static final int rotor_names=0x7f040000;
+        public static final int alpha=0x7f050002;
+        public static final int reflector_names=0x7f050001;
+        public static final int rotor_names=0x7f050000;
     }
     public static final class attr {
     }
@@ -20,32 +20,37 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int encoder=0x7f060001;
-        public static final int enigma_info=0x7f060010;
-        public static final int group_text=0x7f060004;
-        public static final int input=0x7f060002;
-        public static final int output=0x7f060003;
-        public static final int plugboard=0x7f06000f;
-        public static final int settings=0x7f060005;
-        public static final int sim=0x7f060000;
-        public static final int spn_rings_1=0x7f06000c;
-        public static final int spn_rings_2=0x7f06000d;
-        public static final int spn_rings_3=0x7f06000e;
-        public static final int spn_rotors_1=0x7f060006;
-        public static final int spn_rotors_2=0x7f060007;
-        public static final int spn_rotors_3=0x7f060008;
-        public static final int spn_start_1=0x7f060009;
-        public static final int spn_start_2=0x7f06000a;
-        public static final int spn_start_3=0x7f06000b;
+        public static final int encoder=0x7f070001;
+        public static final int enigma_info=0x7f070010;
+        public static final int group_text=0x7f070004;
+        public static final int input=0x7f070002;
+        public static final int output=0x7f070003;
+        public static final int plugboard=0x7f07000f;
+        public static final int settings=0x7f070005;
+        public static final int sim=0x7f070000;
+        public static final int spn_rings_1=0x7f07000c;
+        public static final int spn_rings_2=0x7f07000d;
+        public static final int spn_rings_3=0x7f07000e;
+        public static final int spn_rotors_1=0x7f070006;
+        public static final int spn_rotors_2=0x7f070007;
+        public static final int spn_rotors_3=0x7f070008;
+        public static final int spn_start_1=0x7f070009;
+        public static final int spn_start_2=0x7f07000a;
+        public static final int spn_start_3=0x7f07000b;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class raw {
+        public static final int key_down=0x7f040000;
+        public static final int key_up=0x7f040001;
+        public static final int rotor=0x7f040002;
+    }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int reflector_prompt=0x7f050001;
-        public static final int ring_prompt=0x7f050003;
-        public static final int rotor_prompt=0x7f050002;
-        public static final int start_prompt=0x7f050004;
+        public static final int app_name=0x7f060000;
+        public static final int reflector_prompt=0x7f060001;
+        public static final int ring_prompt=0x7f060003;
+        public static final int rotor_prompt=0x7f060002;
+        public static final int start_prompt=0x7f060004;
     }
 }
