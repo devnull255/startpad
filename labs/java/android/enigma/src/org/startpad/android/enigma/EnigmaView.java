876caf01b22b6f3238dcc07e17f76e20be04b965
Mike Koss <mckoss@startpad.org>
2010-02-08 10:59:40 -0800

    start EnigmaView subclass of View

---
+++ labs/java/android/enigma/src/org/startpad/android/enigma/EnigmaView.java
@@ -0,0 +1,24 @@
+package org.startpad.android.enigma;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class EnigmaView extends View {
+
+	public EnigmaView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EnigmaView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EnigmaView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+}
