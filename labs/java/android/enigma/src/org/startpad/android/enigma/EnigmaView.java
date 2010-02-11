04b4279eb5c6be46cd679e775c5324c49d4487d3
Mike Koss <mckoss@startpad.org>
2010-02-10 16:31:12 -0800

    light display working!

--- labs/java/android/enigma/src/org/startpad/android/enigma/EnigmaView.java
+++ labs/java/android/enigma/src/org/startpad/android/enigma/EnigmaView.java
@@ -7,6 +7,7 @@ import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -26,8 +27,15 @@ public class EnigmaView extends View {
 	int viewHeight;
 	
 	private Rect[] rcRotors = new Rect[3];
+	private Rect rcAllRotors;
 	private Rect[] rcSpinners = new Rect[3];
 	private Rect rcLetters;
+	
+	Qwertzu qLights = new Qwertzu();
+	Qwertzu qKeys = new Qwertzu();
+	
+	boolean fDown = false;
+	char chLight = 0;
 	
 	boolean fLidClosed = true;
 	
@@ -59,6 +67,10 @@ public class EnigmaView extends View {
 			{
 			rcRotors[i] = new Rect(axRotors[i]-dxRotor/2, yRotor-dyRotor/2,
 								   axRotors[i]+dxRotor/2, yRotor+dyRotor/2);
+            if (i == 0)
+                rcAllRotors = new Rect(rcRotors[i]);
+            else
+                rcAllRotors.union(rcRotors[i]);
 			Log.d(TAG, "Rotor: " + rcRotors[i].toString());
 			rcRotors[i].inset((int) (xScale*7), (int) (yScale*20));
 			}
@@ -69,6 +81,34 @@ public class EnigmaView extends View {
 		int dxLetters = (int) (res.getDimension(R.dimen.dx_letters) * xScale);
 		
 		rcLetters = new Rect(xLetters, yLetters, xLetters+dxLetters, yLetters+dyLetters);
+		
+		// Setup dimensions of keyboard for hit testing
+		int xP = (int) (res.getDimension(R.dimen.x_P_key) * xScale);
+		int yP = (int) (res.getDimension(R.dimen.y_P_key) * yScale);
+		int dxKey = (int) (res.getDimension(R.dimen.dx_key) * xScale);
+		int dyKey = (int) (res.getDimension(R.dimen.dy_key) * yScale);
+		Rect rcPKey = new Rect(xP, yP, xP + dxKey, yP + dyKey);
+		
+		int dxRightKey = (int) (res.getDimension(R.dimen.dx_right_key) * xScale);
+		int dxUpKey = (int) (res.getDimension(R.dimen.dx_up_key) * xScale);
+		int dyUpKey = (int) (res.getDimension(R.dimen.dy_up_key) * yScale);
+		Point ptRightKey = new Point(dxRightKey, 0);
+		Point ptUpKey = new Point(dxUpKey, dyUpKey);
+		qKeys.setSize(rcPKey, ptRightKey, ptUpKey);
+		
+		// Setup dimensions of lights for display
+		xP = (int) (res.getDimension(R.dimen.x_P_light) * xScale);
+        yP = (int) (res.getDimension(R.dimen.y_P_light) * yScale);
+        int dxLight = (int) (res.getDimension(R.dimen.dx_light) * xScale);
+        int dyLight = (int) (res.getDimension(R.dimen.dy_light) * yScale);
+        Rect rcPLight = new Rect(xP, yP, xP + dxLight, yP + dyLight);
+        
+        int dxRightLight = (int) (res.getDimension(R.dimen.dx_right_light) * xScale);
+        int dxUpLight = (int) (res.getDimension(R.dimen.dx_up_light) * xScale);
+        int dyUpLight = (int) (res.getDimension(R.dimen.dy_up_light) * yScale);
+        Point ptRightLight = new Point(dxRightLight, 0);
+        Point ptUpLight = new Point(dxUpLight, dyUpLight);
+        qLights.setSize(rcPLight, ptRightLight, ptUpLight);
 		}
 	
 	protected void onDraw(Canvas canvas)
@@ -90,8 +130,14 @@ public class EnigmaView extends View {
 			canvas.drawText(sPosition.substring(i,i+1), rcRotors[i].centerX(), rcRotors[i].bottom, paint);
 			}
 		
-		letters.setBounds(rcLetters);
-		letters.draw(canvas);
+		if (fDown)
+		    {
+		    canvas.save();
+		    canvas.clipRect(qLights.rectFromChar(chLight));
+    		letters.setBounds(rcLetters);
+    		letters.draw(canvas);
+    		canvas.restore();
+		    }
 		}
 
 	public EnigmaView(Context context) {
@@ -122,8 +168,6 @@ public class EnigmaView extends View {
     	
 	    setOnTouchListener(new OnTouchListener()
 	        {
-	        boolean fDown = false;
-	        
 	        public boolean onTouch(View view, MotionEvent event)
 	            {
 	            if (fLidClosed)
@@ -133,22 +177,31 @@ public class EnigmaView extends View {
 	                invalidate(0, 0, viewWidth, viewHeight);
 	                return true;
 	                }
+	            
 	            switch (event.getAction())
 	            {
 	            case MotionEvent.ACTION_DOWN:
-	                if (!fDown)
-	                    {
-	                    fDown = true;
-	                    EnigmaApp.SoundEffect.KEY_DOWN.play();
+	                Point ptClick = new Point((int) event.getX(), (int) event.getY());
+	                char ch = qKeys.charFromPt(ptClick);
+	                if (ch == 0)
+	                    return false;
+
+                    fDown = true;
+
+                    chLight = machine.encodeChar(ch);
+                    Log.d(TAG, "Encode " + ch + " -> " + chLight);
+                    
+                    invalidate(rcAllRotors);
+                    invalidate(qLights.rectFromChar(chLight));
 	                    
-	                    machine.spinRotor(2, 1);
-	                    invalidate(rcRotors[2]);
-	                    }
+                    EnigmaApp.SoundEffect.KEY_DOWN.play();
 	                break;
+
 	            case MotionEvent.ACTION_UP:
 	                if (fDown)
 	                    {
 	                    fDown = false;
+	                    invalidate(qLights.rectFromChar(chLight));
 	                    EnigmaApp.SoundEffect.KEY_UP.play();
 	                    }
 	                break;
@@ -157,4 +210,66 @@ public class EnigmaView extends View {
 	            }
 	        });
 	    }
+    
+    /* Convert from x,y to a keyboard/light character.  Assumes QWERTU
+     * character layout.
+     */
+    
+    private static String[] asRows = new String[] {"QWERTZUIO", "ASDFGHJK", "PYXCVBNML"};
+    
+    class Qwertzu
+        {
+        private Point ptRight;
+        private Rect[] arcRows;
+        
+        public void setSize(Rect rcP, Point ptRight, Point ptUp)
+            {
+            this.ptRight = new Point(ptRight);
+            
+            rcP = new Rect(rcP);
+            
+            Rect rcA = new Rect(rcP);
+            rcA.offset(ptRight.x, ptRight.y);
+            rcA.offset(ptUp.x, ptUp.y);
+            
+            Rect rcQ = new Rect(rcA);
+            rcQ.offset(ptUp.x, ptUp.y);
+            
+            arcRows = new Rect[] {rcQ, rcA, rcP};
+            }
+        
+        public char charFromPt(Point ptClick)
+            {
+            for (int i = 0; i < arcRows.length; i++)
+                {
+                Rect rcChar = new Rect(arcRows[i]);
+                
+                for (int j = 0; j < asRows[i].length(); j++)
+                    {
+                    if (rcChar.contains(ptClick.x, ptClick.y))
+                        return asRows[i].charAt(j);
+                    rcChar.offset(ptRight.x, ptRight.y);
+                    }
+                }
+                
+            return 0;
+            }
+        
+        public Rect rectFromChar(char ch)
+            {
+            for (int i = 0; i < arcRows.length; i++)
+                {
+                Rect rcChar = new Rect(arcRows[i]);
+
+                for (int j = 0; j < asRows[i].length(); j++)
+                    {
+                    if (ch == asRows[i].charAt(j))
+                        return rcChar;
+                    rcChar.offset(ptRight.x, ptRight.y);
+                    }
+                }
+            
+            return null;
+            }
+        }
 }
